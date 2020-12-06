c31bdf8df23669ff63605b8fa2676a9e3f3b1ec7
David Johnson <david.m.johnson32@gmail.com>
2020-12-05 16:33:31 -0800

    Added variables

---
+++ score.js
@@ -0,0 +1,7 @@
+// Setting variables
+
+var storedScores = JSON.parse(localStorage.getItem("userData"));
+var highScoresArea = document.querySelector("#highScoresList");
+var backBtn = document.querySelector("#backButton");
+var clearBtn = document.querySelector("#clearScores");
+
