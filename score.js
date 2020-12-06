5207397235d25e0beb4acb6ad40ff3298e18bbd7
David Johnson <david.m.johnson32@gmail.com>
2020-12-05 16:38:24 -0800

    Added funtion to display scores

--- score.js
+++ score.js
@@ -5,3 +5,23 @@ var highScoresArea = document.querySelector("#highScoresList");
 var backBtn = document.querySelector("#backButton");
 var clearBtn = document.querySelector("#clearScores");
 
+// Creating function to diplay score list 
+
+function displayScores() {
+    if (storedScores !== null) {
+        var scoreList = document.createElement("ol");
+        scoreList.className = "scoreListClass";
+        for (var i = 0; i < storedScores.length; i++) {
+            var initials = storedScores[i].inits;
+            var scores = storedScores[i].userScore
+            var scoreEntry = document.createElement("li");
+            scoreEntry.innerHTML = initials + " - " + scores;
+            scoreList.appendChild(scoreEntry);
+        }
+        highScoresArea.appendChild(scoreList);
+    }
+};
+
+displayScores();
+
+
